dc474b55faa7d5259e70115a387b73b4a933e7a6
thisisaj1999 <atuljoshi94170@gmail.com>
2024-04-30 21:04:32 +0400

    [Add]: Sale container

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -8,6 +8,7 @@ import Card from "../../components/Card";
 import { useAuth } from "../../utils/services/authentication";
 import { useNavigate } from "react-router-dom";
 import { useSnackbar } from "notistack";
+import Sale from "../../assets/Sale.jpg";
 
 const index: React.FC = () => {
 	const Auth = useAuth();
@@ -48,6 +49,18 @@ const index: React.FC = () => {
 	};
 
 	const handleSeeAllBtn = (e: any) => {
+		e.preventDefault();
+		if (Auth.User) {
+			navigate("/dashboard");
+		} else {
+			navigate("/login");
+			enqueueSnackbar("Please sign in to visit shop", {
+				variant: "info",
+			});
+		}
+	};
+
+	const handleSeeOnMapBtn = (e: any) => {
 		e.preventDefault();
 		if (Auth.User) {
 			navigate("/dashboard");
@@ -107,6 +120,34 @@ const index: React.FC = () => {
 				>
 					See All
 				</button>
+			</section>
+
+			{/* Sale */}
+			<section className={styles.SaleContainer}>
+				<div className={styles.SaleImageContainer}>
+					<img src={Sale} alt="sale image" />
+				</div>
+				<div className={styles.SaleContentContainer}>
+					<h1 className={styles.SaleContentHeading}>
+						Find Your Perfect Look at Bolt's Stylish New on Tokyo
+					</h1>
+					<p className={styles.SaleContentSubHeading}>
+						Welcome to the newest Bolt outlet in Shibuya, Japan!
+						Step into our stylish and trendy store and discover the
+						latest in fashion and apparel. Come and experience the
+						unique and vibrant atmosphere.
+					</p>
+					<div className={styles.SaleFlashBatch}>
+						<p className={styles.SaleFlashBatchHeading}>Come and Enjoy Sale!</p>
+						<h1 className={styles.SaleFlashBatchPercentage}>50%</h1>
+					</div>
+					<button
+						className={styles.SeeOnMapsBtn}
+						onClick={(e) => handleSeeOnMapBtn(e)}
+					>
+						See On Maps
+					</button>
+				</div>
 			</section>
 		</main>
 	);

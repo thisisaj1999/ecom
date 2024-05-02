d8c2f5c67c1df2ea752520763c59c6567d003aec
thisisaj1999 <atuljoshi94170@gmail.com>
2024-05-02 23:35:54 +0400

    [Update]: Feature Collections added

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./Home.module.scss";
 import { useGlobalStore } from "../../utils/store";
 import { useLocation } from "react-router-dom";
@@ -8,7 +8,15 @@ import Card from "../../components/Card";
 import { useAuth } from "../../utils/services/authentication";
 import { useNavigate } from "react-router-dom";
 import { useSnackbar } from "notistack";
-import Sale from "../../assets/Sale.jpg";
+import Sale from "../../assets/sale.jpg";
+
+// Feature Collection
+import Accesories from "../../assets/accesories.jpg";
+import Bag from "../../assets/bag.jpg";
+import Bottoms from "../../assets/bottoms.jpg";
+import Footwear from "../../assets/footwear.jpg";
+import Headwear from "../../assets/headwear.jpg";
+import Jacket from "../../assets/jacket.jpg";
 
 const index: React.FC = () => {
 	const Auth = useAuth();
@@ -61,6 +69,18 @@ const index: React.FC = () => {
 	};
 
 	const handleSeeOnMapBtn = (e: any) => {
+		e.preventDefault();
+		if (Auth.User) {
+			navigate("/dashboard");
+		} else {
+			navigate("/login");
+			enqueueSnackbar("Please sign in to visit shop", {
+				variant: "info",
+			});
+		}
+	};
+
+	const handleDiscoverBtn = (e: any) => {
 		e.preventDefault();
 		if (Auth.User) {
 			navigate("/dashboard");
@@ -138,7 +158,9 @@ const index: React.FC = () => {
 						unique and vibrant atmosphere.
 					</p>
 					<div className={styles.SaleFlashBatch}>
-						<p className={styles.SaleFlashBatchHeading}>Come and Enjoy Sale!</p>
+						<p className={styles.SaleFlashBatchHeading}>
+							Come and Enjoy Sale!
+						</p>
 						<h1 className={styles.SaleFlashBatchPercentage}>50%</h1>
 					</div>
 					<button
@@ -147,6 +169,121 @@ const index: React.FC = () => {
 					>
 						See On Maps
 					</button>
+				</div>
+			</section>
+
+			{/* Feature Collections */}
+			<section className={styles.FeatureCollectionsContainer}>
+				<h1 className={styles.FeatureCollectionsHeading}>
+					Feature Collections
+				</h1>
+				<p className={styles.FeatureCollectionsSubHeading}>
+					Dare to mix and match! Check our collections to level up
+					your fashion game
+				</p>
+				<div className={styles.FeatureCollectionsGrid}>
+					<div className={styles.Footwear}>
+						<img src={Footwear} alt="Footwear" />
+						<div className={styles.CardContent}>
+							<p className={styles.CardContentHeading}>
+								Footwear
+							</p>
+							<p className={styles.CardContentSubHeading}>
+								Check our cool footwear collections. Get Disc
+								10% on New Sseason!
+							</p>
+							<button
+								className={styles.DiscoverBtn}
+								onClick={(e) => handleDiscoverBtn(e)}
+							>
+								Discover
+							</button>
+						</div>
+					</div>
+					<div className={styles.Jacket}>
+						<img src={Jacket} alt="Jacket" />
+						<div className={styles.CardContent}>
+							<p className={styles.CardContentHeading}>Jacket</p>
+							<p className={styles.CardContentSubHeading}>
+								Check our cool jacket collections. Get Disc 10%
+								on New Sseason!
+							</p>
+							<button
+								className={styles.DiscoverBtn}
+								onClick={(e) => handleDiscoverBtn(e)}
+							>
+								Discover
+							</button>
+						</div>
+					</div>
+					<div className={styles.Accesories}>
+						<img src={Accesories} alt="Accesories" />
+						<div className={styles.CardContent}>
+							<p className={styles.CardContentHeading}>
+								Accesories
+							</p>
+							<p className={styles.CardContentSubHeading}>
+								Check our cool accesories collections. Get Disc
+								10% on New Sseason!
+							</p>
+							<button
+								className={styles.DiscoverBtn}
+								onClick={(e) => handleDiscoverBtn(e)}
+							>
+								Discover
+							</button>
+						</div>
+					</div>
+					<div className={styles.Headwear}>
+						<img src={Headwear} alt="Headwear" />
+						<div className={styles.CardContent}>
+							<p className={styles.CardContentHeading}>
+								Headwear
+							</p>
+							<p className={styles.CardContentSubHeading}>
+								Check our cool headwear collections. Get Disc
+								10% on New Sseason!
+							</p>
+							<button
+								className={styles.DiscoverBtn}
+								onClick={(e) => handleDiscoverBtn(e)}
+							>
+								Discover
+							</button>
+						</div>
+					</div>
+					<div className={styles.Bag}>
+						<img src={Bag} alt="Bag" />
+						<div className={styles.CardContent}>
+							<p className={styles.CardContentHeading}>Bag</p>
+							<p className={styles.CardContentSubHeading}>
+								Check our cool bag collections. Get Disc 10% on
+								New Sseason!
+							</p>
+							<button
+								className={styles.DiscoverBtn}
+								onClick={(e) => handleDiscoverBtn(e)}
+							>
+								Discover
+							</button>
+						</div>
+					</div>
+					<div className={styles.Bottoms}>
+						<img src={Bottoms} alt="Bottoms" />
+						<div className={styles.CardContent}>
+							<p className={styles.CardContentHeading}>Bottoms</p>
+							<p className={styles.CardContentSubHeading}>
+								Check our cool bottoms collections. Get Disc 10%
+								on New Sseason!
+							</p>
+							<button
+								className={styles.DiscoverBtn}
+								onClick={(e) => handleDiscoverBtn(e)}
+							>
+								Discover
+							</button>
+						</div>
+					</div>
 				</div>
 			</section>
 		</main>

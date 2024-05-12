8317daeee6bed30911230db3622a54a17eb4b4bd
thisisaj1999 <atuljoshi94170@gmail.com>
2024-05-12 21:16:42 +0400

    [Add]: Components Account, Order, Cart

---
+++ src/pages/Cart/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const index: React.FC = () => {
+  return (
+    <div>Cart</div>
+  )
+}
+
+export default index
---
+++ src/pages/Order/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const index: React.FC = () => {
+  return (
+    <div>Order History</div>
+  )
+}
+
+export default index

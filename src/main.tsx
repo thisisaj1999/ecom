8fc2b0f4b02daa281d6378ba6d9d922adfd2b72e
thisisaj1999 <atuljoshi94170@gmail.com>
2024-05-12 19:37:18 +0400

    [Fix]: Reduced the autoHideDuration

--- src/main.tsx
+++ src/main.tsx
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
 			<BrowserRouter>
 				<SnackbarProvider
 					maxSnack={1}
-					autoHideDuration={5000}
+					autoHideDuration={2000}
 					preventDuplicate
 					anchorOrigin={{
 						vertical: "top",

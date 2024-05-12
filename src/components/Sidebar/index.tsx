98560ee21dc9a0d96a6aba0982d94ef5b2fe441c
thisisaj1999 <atuljoshi94170@gmail.com>
2024-05-12 19:37:09 +0400

    [Add]: Icons for Menus

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,24 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styles from "./Sidebar.module.scss";
 
-import User from '../../assets/menu/user.svg'
-import Cart from '../../assets/menu/cart.svg'
-import Order from '../../assets/menu/order.svg'
-import Category from '../../assets/menu/category.svg'
+import User from "../../assets/menu/user.svg";
+import Cart from "../../assets/menu/cart.svg";
+import Order from "../../assets/menu/order.svg";
+import Category from "../../assets/menu/category.svg";
 
-import Bike from '../../assets/menu/bike.svg'
-import Exercise from '../../assets/menu/exercise.svg'
-import Racket from '../../assets/menu/racket.svg'
-import Water from '../../assets/menu/water.svg'
-import Hiking from '../../assets/menu/hiking.svg'
-import Teams from '../../assets/menu/teams.svg'
-import Skates from '../../assets/menu/skates.svg'
-
-
-
+import Bike from "../../assets/menu/bike.svg";
+import Exercise from "../../assets/menu/exercise.svg";
+import Racket from "../../assets/menu/racket.svg";
+import Water from "../../assets/menu/water.svg";
+import Hiking from "../../assets/menu/hiking.svg";
+import Teams from "../../assets/menu/teams.svg";
+import Skates from "../../assets/menu/skates.svg";
 
 import type { MenuProps } from "antd";
 import { Menu } from "antd";
+import { pathSlugMaker, wordsToSlug } from "../../utils/services/other";
+
+import { useGlobalStore } from "../../utils/store";
+import { useNavigate } from "react-router-dom";
 
 type MenuItem = Required<MenuProps>["items"][number];
 
@@ -29,128 +30,151 @@ const items: MenuItem[] = [
 		type: "group",
 		children: [
 			{
-				key: "All_Sports",
+				key: "all-sports",
 				label: "All Sports",
 				icon: <img src={Category} alt="" width={20} />,
 				children: [
 					{
-						key: "Biking",
+						key: "biking",
 						icon: <img src={Bike} alt="" width={22} />,
 						label: "Biking",
 						children: [
-							{ key: "1", label: "Bikes" },
-							{ key: "2", label: "Bikes Helmet" },
-							{ key: "3", label: "Bikes Clothing" },
-							{ key: "4", label: "Bikes Footwear" },
-							{ key: "5", label: "Bike Accessories" },
-							{ key: "6", label: "Bikes Maintenance" },
+							{ key: "bikes", label: "Bikes" },
+							{ key: "bikes-helmet", label: "Bikes Helmet" },
+							{ key: "bikes-clothing", label: "Bikes Clothing" },
+							{ key: "bikes-footwear", label: "Bikes Footwear" },
+							{ key: "bike-accessories", label: "Bike Accessories" },
+							{ key: "bikes-maintenance", label: "Bikes Maintenance" },
 						],
 					},
 					{
-						key: "Exercise",
+						key: "exercise",
 						icon: <img src={Exercise} alt="" width={19} />,
 						label: "Exercise",
 						children: [
-							{ key: "7", label: "Combat" },
-							{ key: "8", label: "Fitness" },
-							{ key: "9", label: "Yoga & Studio" },
-							{ key: "10", label: "Dance" },
-							{ key: "11", label: "Gymnastics" },
+							{ key: "combat", label: "Combat" },
+							{ key: "fitness", label: "Fitness" },
+							{ key: "Yoga-n-Studio", label: "Yoga & Studio" },
+							{ key: "dance", label: "Dance" },
+							{ key: "gymnastics", label: "Gymnastics" },
 						],
 					},
 					{
-						key: "Racket_Sports",
+						key: "racket-sports",
 						icon: <img src={Racket} alt="" width={19} />,
 						label: "Racket Sports",
 						children: [
-							{ key: "12", label: "Table Tennis" },
-							{ key: "13", label: "Badminton" },
-							{ key: "14", label: "Padel" },
-							{ key: "15", label: "Squash" },
-							{ key: "16", label: "Tennis" },
-							{ key: "17", label: "Beach Tennis" },
+							{ key: "table-tennis", label: "Table Tennis" },
+							{ key: "badminton", label: "Badminton" },
+							{ key: "padel", label: "Padel" },
+							{ key: "squash", label: "Squash" },
+							{ key: "tennis", label: "Tennis" },
+							{ key: "beach-tennis", label: "Beach Tennis" },
 						],
 					},
 					{
-						key: "Water_Sports",
+						key: "water-sports",
 						icon: <img src={Water} alt="" width={20} />,
 						label: "Water Sports",
 						children: [
-							{ key: "18", label: "Kayaking" },
-							{ key: "19", label: "Kitesurfing" },
-							{ key: "20", label: "Sailing" },
-							{ key: "21", label: "Snorkeling & Scubadiving" },
-							{ key: "22", label: "Stand Up Paddle" },
-							{ key: "23", label: "Surfing" },
-							{ key: "24", label: "Swimming" },
-							{ key: "25", label: "Beach Tennis" },
+							{ key: "kayaking", label: "Kayaking" },
+							{ key: "kitesurfing", label: "Kitesurfing" },
+							{ key: "sailing", label: "Sailing" },
+							{ key: "snorkeling-n-scubadiving", label: "Snorkeling & Scubadiving" },
+							{ key: "stand-up-paddle", label: "Stand Up Paddle" },
+							{ key: "surfing", label: "Surfing" },
+							{ key: "swimming", label: "Swimming" },
 						],
 					},
 					{
-						key: "Hiking_Camping",
+						key: "hiking-camping",
 						icon: <img src={Hiking} alt="" width={22} />,
 						label: "Hiking & Camping",
 						children: [
-							{ key: "26", label: "Hiking Backpacks" },
-							{ key: "27", label: "Tents & Sleeping" },
-							{ key: "28", label: "Camp Furniture" },
-							{ key: "29", label: "Camp Kitchen" },
-							{ key: "30", label: "Drinking" },
-							{ key: "31", label: "Lighting" },
-							{ key: "32", label: "Hiking Clothing" },
-							{ key: "33", label: "Hiking Footwear" },
-							{ key: "34", label: "Hiking Accessories" },
+							{ key: "hiking-backpacks", label: "Hiking Backpacks" },
+							{ key: "tents-n-sleeping", label: "Tents & Sleeping" },
+							{ key: "camp-furniture", label: "Camp Furniture" },
+							{ key: "camp-kitchen", label: "Camp Kitchen" },
+							{ key: "drinking", label: "Drinking" },
+							{ key: "lighting", label: "Lighting" },
+							{ key: "hiking-clothing", label: "Hiking Clothing" },
+							{ key: "hiking-footwear", label: "Hiking Footwear" },
+							{ key: "hiking-accessories", label: "Hiking Accessories" },
 						],
 					},
 					{
-						key: "Team_Sports",
+						key: "team-sports",
 						icon: <img src={Teams} alt="" width={22} />,
 						label: "Team Sports",
 						children: [
-							{ key: "35", label: "Football" },
-							{ key: "36", label: "Basketball" },
-							{ key: "37", label: "Volleyball" },
-							{ key: "38", label: "Baseball" },
-							{ key: "39", label: "Rugby" },
-							{ key: "40", label: "Team Handball" },
+							{ key: "football", label: "Football" },
+							{ key: "basketball", label: "Basketball" },
+							{ key: "volleyball", label: "Volleyball" },
+							{ key: "baseball", label: "Baseball" },
+							{ key: "rugby", label: "Rugby" },
+							{ key: "team-handball", label: "Team Handball" },
 						],
 					},
 					{
-						key: "Skates_Scooters",
+						key: "skates-scooters",
 						icon: <img src={Skates} alt="" width={20} />,
 						label: "Skates & Scooters",
 						children: [
-							{ key: "41", label: "Roller Skating" },
-							{ key: "42", label: "Scooter" },
-							{ key: "43", label: "Skateboarding" },
+							{ key: "roller-skating", label: "Roller Skating" },
+							{ key: "scooter", label: "Scooter" },
+							{ key: "skateboarding", label: "Skateboarding" },
 						],
 					},
 				],
 			},
 			{
-				key: "Brands",
+				key: "brands",
 				label: "Brands",
 				icon: <img src={Category} alt="" width={20} />,
 				children: [
-					{ key: "44", label: "Quechua" },
-					{ key: "45", label: "Nabaiji" },
-					{ key: "46", label: "Pongori" },
-					{ key: "47", label: "Kipsta" },
-					{ key: "48", label: "Btwin" },
-					{ key: "49", label: "Forclaz" },
-					{ key: "50", label: "Triban" }
+					{ key: "quechua", label: "Quechua" },
+					{ key: "nabaiji", label: "Nabaiji" },
+					{ key: "pongori", label: "Pongori" },
+					{ key: "kipsta", label: "Kipsta" },
+					{ key: "btwin", label: "Btwin" },
+					{ key: "forclaz", label: "Forclaz" },
+					{ key: "triban", label: "Triban" },
 				],
 			},
-			{ key: "51", icon: <img src={Cart} alt="" width={22}/>, label: "Cart" },
-			{ key: "52", icon: <img src={Order} alt="" width={22}/>, label: "Order History" },
-			{ key: "53", icon: <img src={User} alt="" width={22}/>, label: "Account" },
+			{
+				key: "cart",
+				icon: <img src={Cart} alt="" width={22} />,
+				label: "Cart",
+			},
+			{
+				key: "order-history",
+				icon: <img src={Order} alt="" width={22} />,
+				label: "Order History",
+			},
+			{
+				key: "account",
+				icon: <img src={User} alt="" width={22} />,
+				label: "Account",
+			},
 		],
 	},
 ];
 
 const index: React.FC = () => {
+
+	const navigate = useNavigate()
+
+	const Update = {
+		Global: {
+			currentPage: useGlobalStore((State) => State.setCurrentPage),
+		},
+	};
+
 	const onClick: MenuProps["onClick"] = (e) => {
-		console.log("click ", e);
+		const pathSlug = pathSlugMaker(e.keyPath);
+		const slug = wordsToSlug(e.key)
+		Update.Global.currentPage(slug)
+		navigate(`/${pathSlug}`)
 	};
 
 	return (
--- src/utils/services/other.ts
+++ src/utils/services/other.ts
@@ -1,11 +1,23 @@
-
 const getRandomColor = () => {
-  const letters = "0123456789ABCDEF";
-  let color = "#";
-  for (let i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
+	const letters = "0123456789ABCDEF";
+	let color = "#";
+	for (let i = 0; i < 6; i++) {
+		color += letters[Math.floor(Math.random() * 16)];
+	}
+	return color;
 };
 
-export { getRandomColor };
+const pathSlugMaker = (arr: string[]): string => {
+	return arr.reverse().join("/");
+};
+
+const wordsToSlug = (word: string): string =>
+	word.toLowerCase().replace(/\s/g, "-");
+
+const slugToWords = (slug: string): string =>
+	slug
+		.split("-")
+		.map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+		.join(" ");
+
+export { getRandomColor, pathSlugMaker, wordsToSlug, slugToWords };

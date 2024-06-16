9cd6929e9e8eeb15e65d947bc608bcfa8a29f8bc
thisisaj1999 <atuljoshi94170@gmail.com>
2024-06-16 21:12:22 +0400

    [Removed]: Role

--- src/contracts/IUserSlice.ts
+++ src/contracts/IUserSlice.ts
@@ -2,9 +2,7 @@ export interface IUserSlice {
   id : string,
   name : string,
   email : string,
-  role : string,
   setId : (id: string) => void,
   setName : (name: string) => void,
   setEmail : (email: string) => void,
-  setRole : (role: string) => void,
 }
--- src/utils/store/slices/userSlice.ts
+++ src/utils/store/slices/userSlice.ts
@@ -5,9 +5,7 @@ export const useUserStore = create<IUserSlice>()((set) => ({
   id : "",
   name : "",
   email : "",
-  role : "",
   setId : (id) => set((state) => ({...state, id })),
   setName : (name) => set((state) => ({...state, name })),
   setEmail : (email) => set((state) => ({...state, email })),
-  setRole : (role) => set((state) => ({...state, role })),
 }))

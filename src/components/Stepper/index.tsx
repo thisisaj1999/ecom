b21d0abd6e55cc663d550615141d3cd7fdd3e1b7
thisisaj1999 <atuljoshi94170@gmail.com>
2024-07-06 11:23:28 +0400

    [Update]: Empty cart function for successful payment

--- src/components/Stepper/index.tsx
+++ src/components/Stepper/index.tsx
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
 import style from './Stepper.module.scss'
-import { Button, message, Steps } from 'antd';
+import { Button, Steps } from 'antd';
 import { useNavigate } from 'react-router-dom';
+import { emptyCartFn } from '../../utils/services/other';
+import { useAuth } from '../../utils/services/authentication';
+import { useSnackbar } from 'notistack';
 
 interface StepperProps {
   FirstStep: React.FC;
@@ -10,8 +13,14 @@ interface StepperProps {
 }
 
 const index: React.FC<StepperProps> = ({FirstStep, SecondStep, ThirdStep}):any => {
+  
+  const Auth = useAuth();
+	const Uid = Auth.User?.uid;
   const navigate = useNavigate()
+  const { enqueueSnackbar } = useSnackbar();
+
   const [current, setCurrent] = useState(0);
+  const [paymentLoading, setPaymentLoading] = useState(false);
 
   const steps = [
     {
@@ -40,6 +49,26 @@ const index: React.FC<StepperProps> = ({FirstStep, SecondStep, ThirdStep}):any =
     }
   };
 
+  const done = async () => {
+    try {
+      setPaymentLoading(true);
+
+      await new Promise<void>((resolve) => {
+        setTimeout(() => {
+          setPaymentLoading(false);
+          enqueueSnackbar("Order Confirmed!", {variant: "success"});
+          emptyCartFn(Uid)
+          resolve();
+
+        }, 2000);
+      });
+
+      navigate('/dashboard');
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
   const items = steps.map((item) => ({ key: item.title, title: item.title }));
 
 
@@ -50,17 +79,17 @@ const index: React.FC<StepperProps> = ({FirstStep, SecondStep, ThirdStep}):any =
         <div className={style.CheckoutComponent}>{steps[current].content}</div>
         <div className={style.CheckoutFooter}>
           {current >= 0 && (
-            <Button onClick={() => prev()}>
+            <Button onClick={prev}>
               Back
             </Button>
           )}
           {current === steps.length - 1 && (
-            <Button type="primary" onClick={() => message.success('Processing complete!')}>
+            <Button type="primary" onClick={done} loading={paymentLoading}>
               Done
             </Button>
           )}
           {current < steps.length - 1 && (
-            <Button type="primary" onClick={() => next()}>
+            <Button type="primary" onClick={next}>
               Next
             </Button>
           )}

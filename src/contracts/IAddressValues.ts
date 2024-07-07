552f691a636acc188b5015e9ad7b5c99aec6d819
thisisaj1999 <atuljoshi94170@gmail.com>
2024-07-07 19:32:49 +0400

    [Add]: Interface for Address

---
+++ src/contracts/IAddressValues.ts
@@ -0,0 +1,10 @@
+export interface IAddressValues {
+  fName: string;
+  lName: string;
+  addressLine1: string;
+  addressLine2?: string;
+  city: string;
+  state: string;
+  country: string;
+  pincode: string;
+}

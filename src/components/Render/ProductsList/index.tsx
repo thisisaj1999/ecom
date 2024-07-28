0aa0ad9db06f0ecc1df99d279ba50275dfaed368
thisisaj1999 <atuljoshi94170@gmail.com>
2024-07-28 13:47:18 +0400

    [Add]: New dynamic products

--- src/components/Render/ProductsList/index.tsx
+++ src/components/Render/ProductsList/index.tsx
@@ -1,14 +1,28 @@
-import React from 'react'
-import style from './ProductsList.module.scss'
+import React from "react";
+import style from "./ProductsList.module.scss";
+import { useDashboardStore } from "../../../utils/store";
+import { IItemsData } from "../../../contracts/IItemsData";
+import Card from "../../Card";
 
-const index: React.FC  = () => {
-  return (
-    <div className={style.ProductsListMain}>
-      <h1 className={style.ProductsListMainHeading}>
-				Products
-			</h1>
-    </div>
-  )
-}
+const index: React.FC = () => {
+	const State = {
+		Dashboard: {
+			itemsData: useDashboardStore((State) => State.itemsData),
+		},
+	};
 
-export default index
+	return (
+		<div className={style.ProductsListMain}>
+			<h1 className={style.ProductsListMainHeading}>Quechua</h1>
+			<div className={style.ProductsListSwiper}>
+				<div className={style.ProductsListSwiperRow}>
+					{State.Dashboard.itemsData.map((item: IItemsData) => (
+            <Card data={item} />
+					))}
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default index;

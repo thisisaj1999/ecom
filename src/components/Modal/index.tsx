6cf291b7e3e2fe59c14c44e6d73d217bc8520983
thisisaj1999 <atuljoshi94170@gmail.com>
2024-07-29 20:25:47 +0400

    [Update]: Profile UI

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,7 +1,10 @@
 import React  from "react";
 import styles from "./Modal.module.scss";
-import { Modal } from "antd";
+import { Modal, Avatar, Form, Input } from "antd";
+import { UserOutlined } from '@ant-design/icons';
 import { useModalStore } from "../../utils/store";
+
+import { IAddressValues } from "../../contracts/IAddressValues";
 
 const index: React.FC = () => {
 
@@ -32,6 +35,10 @@ const index: React.FC = () => {
 		Update.Modal.modalName("");
 	};
 
+	const onValuesChange = (changedValues: Partial<IAddressValues>, allValues: IAddressValues) => {
+    console.log('Form Values:', allValues);
+  };
+
 	return (
 		<>
 			<Modal
@@ -41,9 +48,139 @@ const index: React.FC = () => {
 				onCancel={handleCancel}
 				onOk={handleOk}
 				className={styles.ModalMainContainer}
-				width={1000}
+				width={800}
 			>
-				<p>{'modalText'}</p>
+				
+				<Form
+          layout="vertical"
+          style={{
+            width: '90%'
+          }}
+          onValuesChange={onValuesChange}
+          autoComplete="on"
+        >
+
+					<div className={styles.ProfileAvatarMain}>
+						<Avatar className={styles.ProfileAvatar} icon={<UserOutlined />} />
+					</div>
+          
+					<div className={styles.CardFieldsLayout}>
+            <Form.Item
+              style={{width: '100%'}}
+              label="First Name"
+              name="fName"
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input your First Name',
+                },
+              ]}
+            >
+              <Input style={{height: '40px'}} type="text" placeholder="John" />
+            </Form.Item>
+
+            <Form.Item
+              style={{width: '100%'}}
+              label="Last Name"
+              name="lName"
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input your Last Name',
+                },
+              ]}
+            >
+              <Input style={{height: '40px'}} type="text" placeholder="Doe" />
+            </Form.Item>
+          </div>
+
+          <div className={styles.CardFieldsLayout}>
+            <Form.Item
+              style={{width: '100%'}}
+              label="Address Line 1"
+              name="addressLine1"
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input your Address',
+                },
+              ]}
+            >
+              <Input style={{height: '40px'}} type="text" placeholder="#123, ABC Building" />
+            </Form.Item>
+          </div>
+
+          <div className={styles.CardFieldsLayout}>
+            <Form.Item
+              style={{width: '100%'}}
+              label="Address Line 2"
+              name="addressLine2"
+            >
+              <Input style={{height: '40px'}} type="text" />
+            </Form.Item>
+          </div>
+
+          <div className={styles.CardFieldsLayout}>
+            <Form.Item
+              style={{width: '100%'}}
+              label="City"
+              name="city"
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input your City',
+                },
+              ]}
+            >
+              <Input style={{height: '40px'}} type="text"/>
+            </Form.Item>
+
+            <Form.Item
+              style={{width: '100%'}}
+              label="State"
+              name="state"
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input your state',
+                },
+              ]}
+            >
+              <Input style={{height: '40px'}} type="text"/>
+            </Form.Item>
+          </div>
+         
+          <div className={styles.CardFieldsLayout}>
+            <Form.Item
+              style={{width: '100%'}}
+              label="Country"
+              name="country"
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input your Country',
+                },
+              ]}
+            >
+              <Input style={{height: '40px'}} type="text"/>
+            </Form.Item>
+
+            <Form.Item
+              style={{width: '100%'}}
+              label="Pin Code"
+              name="pincode"
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input your Pin Code',
+                },
+              ]}
+            >
+              <Input style={{height: '40px'}} type="text"/>
+            </Form.Item>
+          </div>
+
+        </Form>
 			</Modal>
 		</>
 	);
